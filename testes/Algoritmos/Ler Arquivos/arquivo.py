807f37f047fed5e7a1d666d187bb8a0ca30b4cd2
Thiago Marques Sousa <50245545+thiagomars@users.noreply.github.com>
2023-10-05 22:29:40 -0300

    Arquivos prontos

---
+++ testes/Algoritmos/Ler Arquivos/arquivo.py
@@ -0,0 +1,13 @@
+# Leitura do arquivo
+def ler_dados_arquivo(nome_arquivo):
+    dados = []
+    with open(nome_arquivo, 'r') as arquivo:
+        # percorres todas as linhas do arquivo
+        for linha in arquivo:
+            # iniciando na linha que começa com a letra E e a ignorando
+            if linha.startswith('E'):
+                valores = linha.split()[1:]
+                if len(valores) == 3:
+                    x, y, z = map(float, valores)
+                    dados.append([x, y, z])
+    return dados
